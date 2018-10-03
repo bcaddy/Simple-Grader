3001b5ab750cf029f8d06a44f0dd607acab23b48
Bob <r.Caddy@pitt.edu>
2018-10-03 15:26:04 -0400

    Version 2.0: Significant increases in robustness, readability, and utility
    - Complete refactoring of version 1.0 to enhance readability and maintainability (Resolves issue #1)
    - A host of checks added to make sure that user input data is correct and to avoid runtime errors (Resolves issues #5 and #9)
    - Added case insensitivity to searches (Resolves issue #2)
    - Added intermediate saves between grading each assignment to avoid losing work if the program crashes (Resolves issue #3)
    - Shows the name of the student when inputting the grades, general UI clean up (Resolves issues #4 and #7)
    - Now sets ungraded students grades to zero after finishing a particular assignment instead of after grading the first student (Resolves issue #6)
    
    Resolves: #1, #2, #3, #4, #5, #6, #7, #9

--- Simple Grader.py
+++ Simple Grader.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """
-    Author: Robert Caddy <rcaddy@psu.edu>
+    Author: Robert Caddy <r.caddy@pitt.edu>
     Created: 9/11/2018
-    Modified: 9/11/201
+    Modified: 10/03/201
     Short Description: Accelerate the grading process of assignments fomr Courseweb
     Long Description:8
         This utility is used to accelerate an instructor's grading process. Reads in a single
@@ -20,94 +20,233 @@
 """
 
 import pandas as pd
-import numpy as np
+from numpy import isnan
 
 
 def main():
-    # get the path to the file
-    inputpath = input('What is the path to the file? ')
-
-    # read in the grades
-    grades = pd.read_csv(inputpath)
+    # import the original grades and find the save path
+    global grades
+    savepath, grades = File_Reader()
 
     #outer loop for choosing which assignment to grade
     while True:
-        # get the assignment name
-        assignment = input('Input the name of the assignment to be graded, type {exit} when done\n ')
-
-        # exit if the user is done grading
-        if assignment.lower() == '{exit}':
+        # get the assignment details
+        assignment, graderange_min, graderange_max = Assignment_Info()
+        
+        if assignment == '{exit}':
             break
 
-        graderange_min = float(input('input the lower limit for the grade: '))
-        graderange_max = float(input('input the upper limit for the grade: '))
-
-
-        # main loop, searches through names, requests grade, writes grade to the grades
-        #  data frame
+        # main loop, searches through names, requests grade, writes grade to the grades data frame
         while True:
-            # get the name of the student
-            name = input('Please input the name of the student, type {exit} when done\n')
+            # find the student
+            student_index = Student_Finder()
 
-            # exit if the user is done with this assignment
-            if name.lower() == '{exit}':
+            # exit the assignment if the user is done with this assignment
+            if student_index == '{exit}':
                 break
+            
+            # grade the student
+            Grader(student_index, assignment, graderange_min, graderange_max)
+            
+        ##### end of student loop #####
+        
+        # Give zeroes to all ungraded students for this assignment
+        for i in range(len(grades)):
+            if isnan(grades.at[i, assignment]):
+                grades.at[i, assignment] = 0
 
-            # find the entries relating to the student in question
-            student = grades[grades['Last Name'].str.contains(name)]
+        # save the grades to a .csv file
+        grades.to_csv(savepath, index=False)
+    
+# =============================================================================
+# 
+# =============================================================================
+    
+def File_Reader():
+    """This function find the path to the original csv file, loads in the grades,
+    and gets the path to save the file at"""
+    
+    while True:
+        # get the path to the file
+        inputpath = input('\nWhat is the path to the file? ')
+    
+        # read in the grades and declare the grades variable to be global so all functions can access it
+        try:
+            raw_grades = pd.read_csv(inputpath)
+            break
+        except FileNotFoundError:
+            print('\nThe file does not exist.  Please try again')
+            
+    while True:
+        # get the path to save the file to
+        savepath = input('\nPlease input the save path: ')
+    
+        # check to see if the path exists
+        try:
+            raw_grades.to_csv(savepath,index=False)
+            break
+        except FileNotFoundError:
+            print('\nThe save path does not exist.  Please try again')
+            
+    return savepath, raw_grades
+    
+    
+# =============================================================================
+# 
+# =============================================================================
+
+def Assignment_Info():
+    """Finds and returns the min and max values for the grade of a given assignemnt.
+       Also tests for data type conformation"""
+    
+    # get the assignment name and check if the assignment exists
+    while True:
+        try:
+            assignment = input('\nInput the name of the assignment to be graded, type {exit} when done\n ')
+            
+            # return exit code if the user wants to exit
+            if assignment.lower() == '{exit}':
+                return assignment.lower(), 0, 0
+            
+            grades[assignment] # this is the actual line that will throw an error if the assignment doesn't exist
+            break
+        except KeyError:
+            print('\nAssignment does not exist.  Please try again')
+
+
+    # get the minimum grade and check for non float values of input
+    while True:
+        try:
+            graderange_min = float(input('\ninput the lower limit for the grade: '))
+            break
+        except ValueError:
+            print('Invalid input.  Please input a number')
+
+    # get the maximum grade and check for non float values of input            
+    while True:
+        try:
+            graderange_max = float(input('input the upper limit for the grade: '))
+            break
+        except ValueError:
+            print('Invalid input.  Please input a number')    
+    
+    return assignment, graderange_min, graderange_max
+
+# =============================================================================
+# 
+# =============================================================================
+
+def Student_Finder():
+    """Takes a students last name as input and then finds that student.
+       This returns the index of the student that was found"""
+    
+    # get students name from the user and determine if that student exists
+    while True:
+        # get the name of the student
+        last_name = input('\nPlease input the name of the student, type {exit} when done\n')
+        
+        # return exit condition and exit if the user is done grading students
+        if last_name.lower() == '{exit}':
+            return last_name
+    
+        # find the entries relating to the student in question
+        student = Student_Searcher(last_name, grades, 'Last Name')
+        
+        if type(student) == str:
+            print('\nStudent does not exist')
+        else:
+            break
 
 
 
-            # if there's multiple students with the same last name then choose just one
-            if len(student) > 1:
-                # inform the user of the issue
-                print('Multiple students found.  Choose which.')
-                print(student['First Name'])
+    # if there's multiple students with the same last name then choose just one
+    if len(student) > 1:
+        # inform the user of the issue
+        print('\nMultiple students found.  Choose which.\n')
+        # print students first names 
+        for i in student.index:
+            print(student['First Name'][i])
 
-                # make sure the user puts the name in correctly
-                while True:
-                    first_name = input('first name: ')
+        # make sure the user puts the name in correctly
+        while True:
+            first_name = input('first name: ')
 
-                    # find the student
-                    temp = student[student['First Name'].str.contains(first_name)]
+            # find the student
+            temp = Student_Searcher(first_name, student, 'First Name')
 
-                    # check if the student was found correctly
-                    if len(temp) == 1:
-                        student = temp
-                        break
-                    else:
-                        print('Incorrect name, try again')
-
-            # check to see if student exists
-            if len(student) == 1:
-
-                # Prompt the user for the grade and check if it's within the range
-                while True:
-                    new_grade = float(input(f'Please input the new grade (current grade = {student[assignment].item()}): '))
-
-                    # check if the grade is in the right range
-                    if graderange_min <= new_grade <= graderange_max:
-                        # assign the grade to the student
-                        grades.at[student.index[0], assignment] = new_grade
-
-                        break
-                    else:
-                        print('Entry out of range, try again')
-
+            # check if the student was found correctly
+            if type(temp) == str:
+                print('Incorrect input, please try again')
             else:
-                print('Incorrect name, please try again')
+                break
+    
+        # reassign the variable student so that it has only one entry
+        student = temp
+            
+    return student.index[0]
 
-            for i in range(len(grades)):
-                if np.isnan(grades.at[i, assignment]):
-                    grades.at[i, assignment] = 0
+# =============================================================================
+# 
+# =============================================================================
+                
+def Student_Searcher(name, dataset, search_type):
+    """Search for the student.  Currently this is case sensitive but does allow
+    for partial searching"""
+    
+    # search for the student, currently this is case sensitive
+    student = dataset[dataset[search_type].str.contains(name, case=False)]
+    
+    # return error code if the student does not exist
+    if len(student) <= 0:
+        student = "student does not exist"        
+        
 
-        #####  The loops are finished now #####
+    return student
 
-    # write the value to a new csv file
-    savepath = input('Please input the save path: ')
-    grades.to_csv(savepath, index=False)
+# =============================================================================
+# 
+# =============================================================================
+
+def Grader(student_index, assignment, graderange_min, graderange_max):
+    """This function does that actual grading.  It determines a value for the 
+    grades then sets the grade to that in the grades data frame.  It also checks
+    to make sure that the grades are within the bounds specified in the \
+    Assignment_Info function"""
+    
+    # These variables just exist so that the string the the new_grade input
+    # isn't 202 columns long.  I couldn't get multiline f strings to work any 
+    # other way
+    a = f'\nPlease input the new grade for {grades.at[student_index,"First Name"]} '
+    b = f'{grades.at[student_index,"Last Name"]} '
+    c = f'(current grade = {grades.at[student_index,assignment]}): '
+    
+    # Prompt the user for the grade and check if it's within the range
+    while True:
+        new_grade = input(f'{a}{b}{c}')
+
+        try:
+            new_grade = float(new_grade)
+        except ValueError:
+            print('\nNon numerical value input.  Please try again')
+            continue
 
 
+        # check if the grade is in the right range
+        if graderange_min <= new_grade <= graderange_max:
+            # assign the grade to the student
+            grades.at[student_index, assignment] = new_grade
+
+            break
+        else:
+            print('\nEntry out of range, try again')
+            continue
+
+# =============================================================================
+# 
+# =============================================================================
+
+
+# don't execute main if this file is called as a module
 if __name__ == '__main__':
     main()
     exit(0)
