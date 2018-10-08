aad9f3155c9ddea82ae6c23a9169fd73c5a9a0ae
Bob <r.Caddy@pitt.edu>
2018-10-08 13:05:07 -0400

    updated the student name prompt to say "last name" instead of just "name"

--- Simple Grader.py
+++ Simple Grader.py
@@ -2,7 +2,7 @@
 """
     Author: Robert Caddy <r.caddy@pitt.edu>
     Created: 9/11/2018
-    Modified: 10/03/201
+    Modified: 10/08/201
     Short Description: Accelerate the grading process of assignments fomr Courseweb
     Long Description:8
         This utility is used to accelerate an instructor's grading process. Reads in a single
@@ -32,7 +32,7 @@ def main():
     while True:
         # get the assignment details
         assignment, graderange_min, graderange_max = Assignment_Info()
-        
+
         if assignment == '{exit}':
             break
 
@@ -44,12 +44,12 @@ def main():
             # exit the assignment if the user is done with this assignment
             if student_index == '{exit}':
                 break
-            
+
             # grade the student
             Grader(student_index, assignment, graderange_min, graderange_max)
-            
+
         ##### end of student loop #####
-        
+
         # Give zeroes to all ungraded students for this assignment
         for i in range(len(grades)):
             if isnan(grades.at[i, assignment]):
@@ -57,57 +57,57 @@ def main():
 
         # save the grades to a .csv file
         grades.to_csv(savepath, index=False)
-    
+
 # =============================================================================
-# 
+#
 # =============================================================================
-    
+
 def File_Reader():
     """This function find the path to the original csv file, loads in the grades,
     and gets the path to save the file at"""
-    
+
     while True:
         # get the path to the file
         inputpath = input('\nWhat is the path to the file? ')
-    
+
         # read in the grades and declare the grades variable to be global so all functions can access it
         try:
             raw_grades = pd.read_csv(inputpath)
             break
         except FileNotFoundError:
             print('\nThe file does not exist.  Please try again')
-            
+
     while True:
         # get the path to save the file to
         savepath = input('\nPlease input the save path: ')
-    
+
         # check to see if the path exists
         try:
             raw_grades.to_csv(savepath,index=False)
             break
         except FileNotFoundError:
             print('\nThe save path does not exist.  Please try again')
-            
+
     return savepath, raw_grades
-    
-    
+
+
 # =============================================================================
-# 
+#
 # =============================================================================
 
 def Assignment_Info():
     """Finds and returns the min and max values for the grade of a given assignemnt.
        Also tests for data type conformation"""
-    
+
     # get the assignment name and check if the assignment exists
     while True:
         try:
             assignment = input('\nInput the name of the assignment to be graded, type {exit} when done\n ')
-            
+
             # return exit code if the user wants to exit
             if assignment.lower() == '{exit}':
                 return assignment.lower(), 0, 0
-            
+
             grades[assignment] # this is the actual line that will throw an error if the assignment doesn't exist
             break
         except KeyError:
@@ -122,36 +122,36 @@ def Assignment_Info():
         except ValueError:
             print('Invalid input.  Please input a number')
 
-    # get the maximum grade and check for non float values of input            
+    # get the maximum grade and check for non float values of input
     while True:
         try:
             graderange_max = float(input('input the upper limit for the grade: '))
             break
         except ValueError:
-            print('Invalid input.  Please input a number')    
-    
+            print('Invalid input.  Please input a number')
+
     return assignment, graderange_min, graderange_max
 
 # =============================================================================
-# 
+#
 # =============================================================================
 
 def Student_Finder():
     """Takes a students last name as input and then finds that student.
        This returns the index of the student that was found"""
-    
+
     # get students name from the user and determine if that student exists
     while True:
         # get the name of the student
-        last_name = input('\nPlease input the name of the student, type {exit} when done\n')
-        
+        last_name = input('\nPlease input the last name of the student, type {exit} when done\n')
+
         # return exit condition and exit if the user is done grading students
         if last_name.lower() == '{exit}':
             return last_name
-    
+
         # find the entries relating to the student in question
         student = Student_Searcher(last_name, grades, 'Last Name')
-        
+
         if type(student) == str:
             print('\nStudent does not exist')
         else:
@@ -163,7 +163,7 @@ def Student_Finder():
     if len(student) > 1:
         # inform the user of the issue
         print('\nMultiple students found.  Choose which.\n')
-        # print students first names 
+        # print students first names
         for i in student.index:
             print(student['First Name'][i])
 
@@ -179,47 +179,47 @@ def Student_Finder():
                 print('Incorrect input, please try again')
             else:
                 break
-    
+
         # reassign the variable student so that it has only one entry
         student = temp
-            
+
     return student.index[0]
 
 # =============================================================================
-# 
+#
 # =============================================================================
-                
+
 def Student_Searcher(name, dataset, search_type):
     """Search for the student.  Currently this is case sensitive but does allow
     for partial searching"""
-    
+
     # search for the student, currently this is case sensitive
     student = dataset[dataset[search_type].str.contains(name, case=False)]
-    
+
     # return error code if the student does not exist
     if len(student) <= 0:
-        student = "student does not exist"        
-        
+        student = "student does not exist"
+
 
     return student
 
 # =============================================================================
-# 
+#
 # =============================================================================
 
 def Grader(student_index, assignment, graderange_min, graderange_max):
-    """This function does that actual grading.  It determines a value for the 
+    """This function does that actual grading.  It determines a value for the
     grades then sets the grade to that in the grades data frame.  It also checks
     to make sure that the grades are within the bounds specified in the \
     Assignment_Info function"""
-    
+
     # These variables just exist so that the string the the new_grade input
-    # isn't 202 columns long.  I couldn't get multiline f strings to work any 
+    # isn't 202 columns long.  I couldn't get multiline f strings to work any
     # other way
     a = f'\nPlease input the new grade for {grades.at[student_index,"First Name"]} '
     b = f'{grades.at[student_index,"Last Name"]} '
     c = f'(current grade = {grades.at[student_index,assignment]}): '
-    
+
     # Prompt the user for the grade and check if it's within the range
     while True:
         new_grade = input(f'{a}{b}{c}')
@@ -242,7 +242,7 @@ def Grader(student_index, assignment, graderange_min, graderange_max):
             continue
 
 # =============================================================================
-# 
+#
 # =============================================================================
 
 
